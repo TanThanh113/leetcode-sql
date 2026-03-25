4751fd55330ea198f21ba0fa0405617f63b093a6
TanThanh113 <thanhday247@gmail.com>
2026-03-25 12:20:25 +0700

    Add solution for Combine Two Tables (LEFT JOIN)

---
+++ combine-two-tables.sql
@@ -0,0 +1,11 @@
+-- LeetCode: Combine Two Tables
+-- Level: Easy
+
+SELECT firstName, lastName, city, state
+FROM person
+left join address
+on person.personId = address.personId
+
+-- Explanation:
+-- Use LEFT JOIN to keep all records from Person
+-- If no matching Address, city and state will be NULL
