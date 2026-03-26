6e8e5dceefe35729efbb2235ce88dabdbfcf60ef
TanThanh113 <thanhday247@gmail.com>
2026-03-26 10:39:26 +0700

    Add solution for Combine Two Tables (LEFT JOIN)

--- easy/combine-two-tables.sql
+++ easy/combine-two-tables.sql
@@ -9,3 +9,6 @@ on person.personId = address.personId
 -- Explanation:
 -- Use LEFT JOIN to keep all records from Person
 -- If no matching Address, city and state will be NULL
+
+-- Concept:
+-- - LEFT JOIN
